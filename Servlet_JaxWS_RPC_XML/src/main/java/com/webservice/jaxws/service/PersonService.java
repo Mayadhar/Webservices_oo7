9d312f4a118f4d59af41cc869fc6c2e2c4c6a05e
Mayadhar Sankhua <mayadhar.sankhua@evry.com>
2019-06-23 21:32:41 +0530

    An example of webservice using apache axis 2 with with basic servlet settings.

---
+++ Servlet_JaxWS_RPC_XML/src/main/java/com/webservice/jaxws/service/PersonService.java
@@ -0,0 +1,25 @@
+package com.webservice.jaxws.service;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+import com.webservice.jaxws.beans.Person;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface PersonService {
+
+	@WebMethod
+	public boolean addPerson(Person p);
+
+	@WebMethod
+	public boolean deletePerson(int id);
+
+	@WebMethod
+	public Person getPerson(int id);
+
+	@WebMethod
+	public Person[] getAllPersons();
+
+}
